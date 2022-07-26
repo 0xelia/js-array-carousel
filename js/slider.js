96df4cf4d9e97c9465d4611f66c48b54f9ab9220
0xelia <elia.vanon@yahoo.com>
2022-07-26 17:35:08 +0200

    milestone 1

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,35 @@
-console.log('ciao')
+const slides = [
+	'./img/01.jpg',
+	'./img/02.jpg',
+	'./img/03.jpg',
+	'./img/04.jpg',
+	'./img/05.jpg',
+]
+
+let wrapperEl = document.querySelector('.slider .slides-wrapper')
+
+// PER OGNI stringa dell'array slides
+for (let i = 0; i < slides.length; i++) {
+    let img = document.createElement('img')
+    img.src = slides[i]
+    
+
+    let liElement = document.createElement('li')
+    liElement.className = 'slide'
+
+    if (i === 0) {
+        liElement.classList.add('active')
+    }
+
+    liElement.append(img)
+    wrapperEl.append(liElement)
+}
+    //creare un tag img 
+    // assegnarli um src
+    // creare un li e assegnargli la classe .slide
+        //SE è il primo 
+            //allora assegnarli la classe .active
+
+
+
+
